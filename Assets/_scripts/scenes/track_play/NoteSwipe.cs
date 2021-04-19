3466c2cb07eb2f229d44dae4ad97dbd7ed5a4836
gohoon-k <herokun.user@gmail.com>
2021-04-19 10:01:47 +0900

    changed handling input process

--- Assets/_scripts/scenes/track_play/NoteSwipe.cs
+++ Assets/_scripts/scenes/track_play/NoteSwipe.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NoteSwipe : Note {
 
     private SpriteRenderer _noteRenderer;
+
+    private float _judgeTime = -1;
 
     new void Start() {
         base.Start();
@@ -28,37 +28,24 @@ public class NoteSwipe : Note {
     protected override void PlayDestroyAnim() {
         ScaleAnim();
         AlphaAnim(_noteRenderer);
-        // AlphaAnim(_arrowRenderer);
-        // ArrowAnim();
     }
 
-    // private void ArrowAnim() {
-    //     var swipeDestination = swipeDirection switch {
-    //         0 => new Vector3(0, 0.5f, 0),
-    //         1 => new Vector3(0.5f, 0, 0),
-    //         2 => new Vector3(0, -0.5f, 0),
-    //         3 => new Vector3(-0.5f, 0, 0),
-    //         _ => new Vector3(0, 0, 0)
-    //     };
-    //     StartCoroutine(Interpolators.Linear(_arrow.transform.position, swipeDestination, 0.2f,
-    //             step => { _arrow.transform.position = step; }, 
-    //             () => { }
-    //         )
-    //     );
-    // }
-
     protected override void HandleInput(Touch touch) {
-        if (touch.phase != TouchPhase.Moved) return;
+        if (touch.phase != TouchPhase.Began && touch.phase != TouchPhase.Moved) return;
         
         var inputPosition = GetInputPosition(touch);
 
         if (!IsTargeted(inputPosition.x)) return;
         if (IsHiddenByOtherNote(inputPosition.x)) return;
 
-        if (!hasInput)
+        if (touch.phase == TouchPhase.Began && !hasInput) {
             StartCoroutine(GiveInput());
-        
-        Judge();
+            _judgeTime = G.InGame.Time;
+        }
+
+        if (touch.phase == TouchPhase.Moved && _judgeTime > 0) {
+            Judge(_judgeTime);
+        }
     }
 
     protected override int DifferenceToJudge(float diff) {
