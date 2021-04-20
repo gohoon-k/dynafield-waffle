45cf9b1b67f2b800293c851a1bc11ff9add2e5c0
jsh <acacia8521@gmail.com>
2021-04-20 16:22:37 +0900

    added function : Back

--- Assets/_scripts/scenes/track_select/TrackSelect.cs
+++ Assets/_scripts/scenes/track_select/TrackSelect.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TrackSelect : MonoBehaviour
 {
+
+
+
+
+
+
+
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
+        G.InitTracks();
         
     }
 
