e1cabdd7162422286a8412b8794cafd2842bd32a
gohoon-k <herokun.user@gmail.com>
2021-04-22 16:06:37 +0900

    added click animation of background effect, and some other things

--- Assets/_scripts/scenes/intro/InputDetector.cs
+++ Assets/_scripts/scenes/intro/InputDetector.cs
@@ -5,25 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class InputDetector : MonoBehaviour {
 
+    public Animator backgroundEffect;
     public ScreenTranslator translator;
+
+    private bool _hasInput = false;
     
     void Update() {
+
+        if (Time.timeSinceLevelLoad <= 5 || _hasInput) return;
 
         if (Input.GetKeyDown(KeyCode.Escape)) {
             Application.Quit();
         }
 
-        if (Input.GetMouseButtonDown(0)) {
-            TranslateToSelect();
-        }
-
-        if (Input.touches.Length > 0) {
-            TranslateToSelect();
+        if (Input.GetMouseButtonDown(0) || Input.touches.Length > 0) {
+            StartCoroutine(TranslateToSelect());
         }
 
     }
 
-    private void TranslateToSelect() {
+    private IEnumerator TranslateToSelect() {
+        _hasInput = true;
+        backgroundEffect.Play("intro_effect_clicked", -1, 0);
+        yield return new WaitForSeconds(0.75f);
         StartCoroutine(translator.ShowTransition(() => { SceneManager.LoadScene("TrackSelect"); }));
     }
     
