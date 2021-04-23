3f3c8c7466ccd51bf4f82d962aa5c8d82fc813ae
gohoon-k <herokun.user@gmail.com>
2021-04-23 15:17:32 +0900

    added intro music and changed input receiving method

--- Assets/_scripts/scenes/intro/InputDetector.cs
+++ Assets/_scripts/scenes/intro/InputDetector.cs
@@ -1,34 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InputDetector : MonoBehaviour {
-
     public Animator backgroundEffect;
     public ScreenTranslator translator;
 
-    private bool _hasInput = false;
-    
-    void Update() {
+    public RectTransform trackSource;
+    public Text trackSourceText;
+    public Image trackSourceLinkUnderline;
+    public Text trackSourceLinkText;
+    public Button trackSourceLink;
 
+    public AudioSource introAudioPlayer;
+
+    private bool _hasInput;
+    private bool _trackSourceShowing;
+
+    private void Start() {
+        StartCoroutine(PlayIntroAudio());
+    }
+
+    void Update() {
         if (Time.timeSinceLevelLoad <= 5 || _hasInput) return;
 
         if (Input.GetKeyDown(KeyCode.Escape)) {
             Application.Quit();
         }
-
-        if (Input.GetMouseButtonDown(0) || Input.touches.Length > 0) {
-            StartCoroutine(TranslateToSelect());
-        }
-
     }
 
     private IEnumerator TranslateToSelect() {
-        _hasInput = true;
         backgroundEffect.Play("intro_effect_clicked", -1, 0);
-        yield return new WaitForSeconds(0.75f);
+        yield return new WaitForSeconds(0.15f);
+        StartCoroutine(Interpolators.Linear(1, 0, 0.65f, step => {
+            introAudioPlayer.volume = step;
+        }, () => { introAudioPlayer.Stop(); }));
         StartCoroutine(translator.ShowTransition(() => { SceneManager.LoadScene("TrackSelect"); }));
     }
-    
+
+    private IEnumerator PlayIntroAudio() {
+        yield return new WaitForSeconds(0.75f);
+        StartCoroutine(Interpolators.Linear(0, 1, 5f, step => {
+            introAudioPlayer.volume = step;
+        }, () => { }));
+        introAudioPlayer.time = 20.2f;
+        introAudioPlayer.Play();
+    }
+
+    public void ToggleTrackSource() {
+        if (!_trackSourceShowing) {
+            trackSource.anchoredPosition = new Vector2(-70, 170);
+            trackSourceLink.interactable = true;
+        }
+
+        StartCoroutine(Interpolators.Linear(!_trackSourceShowing ? 0 : 1f, !_trackSourceShowing ? 1 : 0, 0.25f,
+                step => {
+                    trackSourceLinkText.color = new Color(1, 1, 1, step * 0.8f);
+                    trackSourceText.color = new Color(1, 1, 1, step * 0.95f);
+                    trackSourceLinkUnderline.color = new Color(1, 1, 1, step * 0.8f);
+                },
+                () => {
+                    if (_trackSourceShowing) return;
+                    
+                    trackSourceLink.interactable = false;
+                    trackSource.anchoredPosition = new Vector2(1070, 170);
+                }
+            )
+        );
+
+        _trackSourceShowing = !_trackSourceShowing;
+    }
+
+    public void ScreenTouched() {
+        if (Time.timeSinceLevelLoad <= 5 || _hasInput) return;
+        
+        _hasInput = true;
+        StartCoroutine(TranslateToSelect());
+    }
 }
