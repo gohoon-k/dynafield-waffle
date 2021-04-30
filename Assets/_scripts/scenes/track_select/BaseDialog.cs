4c651379dfc0f151f18b9a1ab12f1bd0f3b6b049
gohoon-k <herokun.user@gmail.com>
2021-04-30 23:04:03 +0900

    Added simple base dialog

---
+++ Assets/_scripts/scenes/track_select/BaseDialog.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseDialog : MonoBehaviour {
+
+    public GameObject dialogs;
+
+    public RectTransform content;
+    public Image background;
+    public Text message;
+    public Text positive;
+    public Text negative;
+
+    public bool isOpen;
+
+    public void Open() {
+        isOpen = true;
+        
+        background.color = new Color(0, 0, 0, 0);
+        message.color = new Color(1, 1, 1, 0);
+        if (!(positive is null)) {
+            positive.color = new Color(1, 1, 1, 0);
+        }
+
+        if (!(negative is null)) {
+            negative.color = new Color(1, 1, 1, 0);
+        }
+        content.localScale = new Vector3(1.5f, 1.5f, 1);
+        
+        gameObject.SetActive(true);
+        
+        StartCoroutine(Interpolators.Linear(0, 1, 0.4f, step => {
+            background.color = new Color(0, 0, 0, step * 0.6f);
+            message.color = new Color(1, 1, 1, step);
+            if (!(positive is null)) {
+                positive.color = new Color(1, 1, 1, step);
+            }
+
+            if (!(negative is null)) {
+                negative.color = new Color(1, 1, 1, step);
+            }
+            content.localScale = new Vector3(1.5f - step / 2f, 1.5f - step / 2f, 1);
+        }, () => { }));
+    }
+
+    public void Close(bool deactivateDialogs) {
+        StartCoroutine(Interpolators.Linear(1, 0, 0.25f, step => {
+            background.color = new Color(0, 0, 0, step * 0.6f);
+            message.color = new Color(1, 1, 1, step);
+            if (!(positive is null)) {
+                positive.color = new Color(1, 1, 1, step);
+            }
+
+            if (!(negative is null)) {
+                negative.color = new Color(1, 1, 1, step);
+            }
+            content.localScale = new Vector3(1.5f - step / 2f, 1.5f - step / 2f, 1);
+        }, () => {
+            gameObject.SetActive(false);
+            isOpen = false;
+            if (deactivateDialogs)
+                dialogs.SetActive(false);
+        }));
+    }
+    
+}
